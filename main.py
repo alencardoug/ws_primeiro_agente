5e2100e5e24842a5a0bd878a17c4aefaf9caf3dc
doug <alencardoug@gmail.com>
2026-07-21 20:00:14 -0300

    feat(main.py): adicionar agente de IA langchain com pydantic via dotenv

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def main():
-    print("Hello from ws-primeiro-agente!")
+from dotenv import load_dotenv
+from langchain.agents import create_agent
+from langchain.messages import HumanMessage
+from pydantic import BaseModel
+from pprint import pprint
+from typing import List
+
+load_dotenv()
+
+# Example 1: Agent with structured output
 
 
-if __name__ == "__main__":
-    main()
+question = HumanMessage(content="Qual a stack do projeto?")
+agent_structured = create_agent(
+    model="gpt-4.1-mini",
+    system_prompt="Você é um expert em python e IA. Responda com as informações sobre a stack do projeto.",
+)
+
+response_structured = agent_structured.invoke(
+    {"messages": [question]}
+)
+
+print("Output do Agente:")
+stack_info = response_structured["messages"][-1].content
+pprint(stack_info)
