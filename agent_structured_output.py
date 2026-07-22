a7e797c6a7b27e7f59b7b2d69f15f28c8a78359c
doug <alencardoug@gmail.com>
2026-07-22 15:57:57 -0300

    feat(chat/llm_models):add model gpt-5-nano fix(agent_structured)output):query fixed

--- agent_structured_output.py
+++ agent_structured_output.py
@@ -9,17 +9,15 @@ load_dotenv()
 
 # Example 1: Agent with structured output
 
-class CapitalInfo(BaseModel):
-	nome: str
-	populacao: int
-	pontos_turisticos: list[str]
-	economia: str
+class CancerInfo(BaseModel):
+	tipos_de_câncer: list[str]
+	quantidade_de_sítios: int
 
 question = HumanMessage(content="Qual são os tipos de câncer, se fossem tratados em sítios/conjuntos de tratamento em um hospital oncológico?")
 agent_structured = create_agent(
 	model="gpt-5-nano",
 	system_prompt="Você é um expert em oncologia.",
-	response_format=CapitalInfo
+	response_format=CancerInfo
 )
 
 response_structured = agent_structured.invoke(
@@ -27,5 +25,5 @@ response_structured = agent_structured.invoke(
 )
 
 print("Output do Agente:")
-capital_info = response_structured["structured_response"]
-pprint(capital_info.model_dump())
+cancer_info = response_structured["structured_response"]
+pprint(cancer_info.model_dump())
